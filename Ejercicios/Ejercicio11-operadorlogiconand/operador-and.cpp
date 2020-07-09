2a78363086f2eb18432691ea0f50e828dee88634
Roberto Emilio Torres Muñoz <emiltores15@gmail.com>
2020-07-09 16:49:20 -0600

    Otra forma de desarrollar condicion If

--- Ejercicios/Ejercicio11-operadorlogiconand/operador-and.cpp
+++ Ejercicios/Ejercicio11-operadorlogiconand/operador-and.cpp
@@ -6,19 +6,33 @@ int main(int argc, char const *argv[])
 {
 
 int a = 50;
-int b = 70;
-int c = 120; 
+int b = 7;
+int c = 12; 
 
+// if (a > b && a > c)
+// {
+//     cout << "A es el mayor: " << a;
+// } else if (b > a && b > c) 
+// {
+//     cout << "B es el mayor: " << b;
+// }else 
+// {
+//     cout <<"C es el mayor: " << c;
+// }
+ 
 if (a > b && a > c)
 {
     cout << "A es el mayor: " << a;
-} else if (b > a && b > c) 
+} 
+ if (b > a && b > c) 
 {
     cout << "B es el mayor: " << b;
-}else 
-{
-    cout <<"C es el mayor: " << c;
 }
- 
+if (c > a && c>b)
+{
+     cout <<"C es el mayor: " << c;
+}
+
+
     return 0;
 }
