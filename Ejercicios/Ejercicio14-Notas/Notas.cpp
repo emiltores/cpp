a7191b1e6a1e56e73015ab257bfc1aaec7de8826
Roberto Emilio Torres Muñoz <emiltores15@gmail.com>
2020-07-14 15:30:58 -0600

    If anidados segunda forma ejercicio Notas

--- Ejercicios/Ejercicio14-Notas/Notas.cpp
+++ Ejercicios/Ejercicio14-Notas/Notas.cpp
@@ -10,35 +10,36 @@ int main(int argc, char const *argv[])
     cin >> nota;
 
 
-    if (nota > 100 || nota < 0)
-    {
-        cout << "Ingrese una nota entre 0 - 100";
-        return 0;
-    }
-    if (nota >= 95 && nota <= 100)
-    {
+    
+    if (nota >= 95 && nota <= 100) {
         cout << "Obtuviste una A";
-    }
-        if (nota >= 85 && nota <= 94)
-    {
-        cout << "Obtuviste una B";
-    }
-        if (nota >= 75 && nota <= 84)
-    {
-        cout << "Obtuviste una C";
-    }
-        if (nota >= 65 && nota <= 74)
-    {
-        cout << "Obtuviste una D";
-    }
-        if (nota >= 55 && nota <= 64)
-    {
-        cout << "Obtuviste una E";
-    }
-        if (nota < 60)
-    {
-        cout << "Obtuviste una F";
-    }
-
+    
+	}else {
+		if (nota >= 85 && nota <= 94) {
+        	cout << "Obtuviste una B";
+    	
+		}else {
+			if (nota >= 75 && nota <= 84) {
+        		cout << "Obtuviste una C";
+    
+			}else
+				if (nota >= 65 && nota <= 74) {
+        			cout << "Obtuviste una D";
+    		
+				}else {
+					if (nota >= 55 && nota <= 64) {
+     				   cout << "Obtuviste una E";
+    				}else {
+						if (nota < 60) {
+     					   cout << "Obtuviste una F";
+    				}else {
+						if (nota > 100 || nota < 0) {
+					        cout << "Ingrese una nota entre 0 - 100";
+    					}
+					} 
+				}
+			}		
+		}
+	}
     return 0;
 }
