8209384586a70b0c20311f4ae8fbd3a8d8e53a04
Roberto Emilio Torres Muñoz <emiltores15@gmail.com>
2020-07-15 15:25:09 -0600

    Probando Ciclos While

---
+++ Ejercicios/Ejercicio16-Ciclos While/Ciclos-While.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a = 5;
+    int b = 2;
+
+    while (a > b)
+    {
+        cout << "Ciclos" << endl;
+    }
+    
+    return 0;
+}
