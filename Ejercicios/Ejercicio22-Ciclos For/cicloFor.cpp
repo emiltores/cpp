f4cd6798e323ba44027da602fc0e72cb36ec53b7
Roberto Emilio Torres Muñoz <emiltores15@gmail.com>
2020-07-17 14:06:57 -0600

    Agregando ejercicio ciclo for

---
+++ Ejercicios/Ejercicio22-Ciclos For/cicloFor.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+    
+    for (int indice  = 1; indice < 11; indice++)
+    {
+        cout << "Valor de la variable indice" << indice << endl;   
+    }
+    
+    return 0;
+}
