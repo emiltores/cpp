9f7f2f48a2d2fde48345bd599595700b8dcc7862
Roberto Emilio Torres Muñoz <emiltores15@gmail.com>
2020-07-17 22:54:51 -0600

    modificando

--- Ejercicios/Ejercicio25-Suma de pares impares/paresimpares.cpp
+++ Ejercicios/Ejercicio25-Suma de pares impares/paresimpares.cpp
@@ -5,19 +5,32 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     int pares = 0;
+    int impares =0;
+    int totales = 0;
 
     for (int i = 0; i < 10; i++)
     {
         if (i % 2 == 0)
         {
             pares = pares + i;
+        }else {
+            if (i % 2 ==1)
+            {
+                impares = impares + i;
+            }
+            
         }
         
         cout << i << " ";
     }
+    totales = (pares + impares);
     cout << endl;
     cout << endl;
     cout << "Total pares" << pares;
+    cout << "Total impares" << impares;
+    cout << "Total" << totales;
+
+
 
     return 0;
 }
