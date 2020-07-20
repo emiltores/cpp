058634c950b42b7be13b4700e7750fb7313e6df2
Roberto Torres <emiltores15@gmail.com>
2020-07-20 14:30:29 -0600

    agregando calculadora completa

---
+++ Ejercicios/Ejercicio33-Calculadora-con-swich/main.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+
+using namespace std;
+
+
+int sumar(int a, int b)
+{
+    return a + b;
+}
+int restar(int a, int b)
+{
+    return a - b;
+}
+int multiplicar(int a, int b)
+{
+    return a * b;
+}
+int dividir(int a, int b)
+{
+    return a + b;
+}
+int calculadora(int a, int b, char operador)
+{
+    
+   switch (operador)
+    {
+        case '+':
+            return sumar(a, b);
+            break;
+        case '-':
+            return restar(a, b);
+            break;
+        case '*':
+            return multiplicar(a, b);
+            break;
+        case '/':
+            return dividir(a, b);
+            break;
+        
+        default:
+            break;
+    }
+    throw "El Operador No Existe";
+}
+
+int main(int argc, char const *argv[])
+{
+    system("cls");
+    int n1 = 0;
+    int n2 = 0;
+    char operador;
+    int resultado = 0;
+
+    cout << "Ingrese a: " << endl;;
+    cin >> n1;
+    cout << "Ingrese b: " << endl;
+    cin >> n2;
+    cout << "Ingrese el Operador (+, -, *, /) ";
+    cin >> operador;
+
+    cout << endl;
+
+
+    try
+    {
+        resultado = calculadora(n1, n2, operador); 
+        cout << "Resultado de " << n1 << " " << operador << " " << n2 << " es: ";
+        cout << resultado; 
+
+    }
+    catch(const char* error)
+    {
+        cout << error;
+    }
+    
+
+    return 0;
+}
