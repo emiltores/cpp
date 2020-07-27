dde2a73dfa84eea26be59bcf8e6b07442ad308e9
Roberto Torres <emiltores15@gmail.com>
2020-07-26 23:06:04 -0600

    Agregando Repaso 3

---
+++ Ejercicios/Ejercicio38-Repaso4/main.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    // system("cls");
+    
+    // for (int i = 0; i <= 100; i= i + 5)
+    // {
+    //     cout << i << endl;
+
+    //     for (int w =1; w <= 5; w++)
+    //     {
+    //         cout << w << endl;
+    //          }      
+    //             for (int e =1; e <= 5; e++)
+    //             {
+    //                 cout << e << endl;
+
+    //                 if (i == 25)
+    //                 {
+    //                     break;
+    //                 }
+            
+    //     }  
+    //     if (i == 25)
+    //     {
+    //         break;
+    //     }
+    //     if (i == 25)
+    //     {
+    //         break;
+    //     }
+        
+    // }
+    
+    // int i = 5;
+    // while (i <= 100)
+    // {
+    //     cout << i << endl;
+    //     i = i + 5;
+
+    //     int j = 0;
+    //     while (j < 25)
+    //     {
+    //         j++;
+    //     }
+        
+    //     if (i == 25)
+    //     {
+    //         break;
+    //     }
+        
+    // }
+    // int i = 5;
+    // do
+    // {
+    //     cout << i << endl;
+    //     i--;
+    // } while (i == -1);
+
+    // int j = 5;
+    // while (j = -1)
+    // {
+    //     cout << j << endl;
+    //     j--;
+    // }
+    
+    int nota = 69;
+    bool aprobo = nota > 69;
+    if (nota > 69)
+    {
+        aprobo = true;
+    }else
+    {
+        aprobo = false;
+    }
+    
+    
+     cout aprobo;
+    
+    return 0;
+}
