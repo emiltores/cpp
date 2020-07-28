e26f2cb65fdd9fe7badf85ecfb0649eab1246dbd
Roberto Torres <emiltores15@gmail.com>
2020-07-27 23:21:54 -0600

    Agregando Tarea 6

---
+++ Ejercicios/Ejercicio45-Punto-de-venta-parte6/factura.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+double subtotal;
+double impuesto;
+double totalApagar;
+string listaProductos;
+
+void agregarProductos(string descrpcion, int cantidad, double precio)
+{
+    listaProductos = listaProductos + descrpcion + '\n';
+    subtotal = subtotal + (cantidad * precio);
+    impuesto = subtotal * 0.15;
+    totalApagar = subtotal + impuesto;
+
+}
+
+void imprimirFactura()
+{
+    system("cls");
+    cout << "*******" << endl;
+    cout << "FACTURA" << endl;
+    cout << "*******" << endl;
+    cout << endl;
+
+    cout << "Productos: " << endl;
+    cout << listaProductos;
+
+    cout << endl;
+    cout << "Subtotal: " << subtotal << endl << endl;
+    cout << "El Total a Pagar con el Impuesto es: " << totalApagar;
+
+    
+    cout << endl;
+    cout << endl;
+    system("pause");
+}
